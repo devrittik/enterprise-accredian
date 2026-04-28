98215e7448607f53997ae5cae7d1cda8ac2acfb0
Rittik <rittikchakraborty24@gmail.com>
2026-04-29 05:00:27 +0530

    Components Misalignment on Production

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,11 +33,14 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Header/>
-        {children}
+        <Header />
+
+        <main className="flex-1">
+          {children}
+        </main>
+
         <Footer />
         <GlobalEnquiryModal />
-
         <Analytics />
         <SpeedInsights />
       </body>
